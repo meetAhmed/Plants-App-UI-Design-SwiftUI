03465540d6ee8e317df55017fe9e89f298443126
Ahmed Ali <aali.97@yahoo.com>
2023-03-07 14:55:54 +0500

    Initial code push

---
+++ PlantsAppUIDesign/PlantsAppUIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsAppUIDesignApp.swift
+//  PlantsAppUIDesign
+//
+//  Created by Ahmed Ali on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlantsAppUIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
